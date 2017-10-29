2561b22d598e1215ad69b2ebe3ef60bc88a6c515
Caleb <inchristalway-tech4him@yahoo.com>
2017-10-28 20:34:14 -0600

    Support any front-matter format.

--- src/formats/frontmatter.js
+++ src/formats/frontmatter.js
@@ -3,43 +3,47 @@ import TOML from './toml';
 import YAML from './yaml';
 import JSONFormatter from './json';
 
-const parsers = {
-  toml: input => TOML.fromFile(null, input),
-  json: (input) => {
-    let JSONinput = input.trim();
-    // Fix JSON if leading and trailing brackets were trimmed.
-    if (JSONinput.substr(0, 1) !== '{') {
-      JSONinput = '{' + JSONinput;
-    }
-    if (JSONinput.substr(-1) !== '}') {
-      JSONinput = JSONinput + '}';
-    }
-    return JSONFormatter.fromFile(null, JSONinput);
-  },
-  yaml: input => YAML.fromFile(null, input),
-}
+const defaultFrontmatterFormat = "yaml";
 
-function inferFrontmatterFormat(str) {
-  const firstLine = str.substr(0, str.indexOf('\n')).trim();
-  if ((firstLine.length > 3) && (firstLine.substr(0, 3) === "---")) {
-    // No need to infer, `gray-matter` will handle things like `---toml` for us.
-    return;
-  }
-  switch (firstLine) {
-    case "---":
-      return { language: "yaml", delimiters: "---" };
-    case "+++":
-      return { language: "toml", delimiters: "+++" };
-    case "{":
-      return { language: "json", delimiters: ["{", "}"] };
-    default:
-      throw "Unrecognized front-matter format.";
-  }
+const parsers = {
+  toml: {
+    parse: input => TOML.fromFile(null, input),
+    stringify: output => TOML.toFile(null, output),
+  },
+  json: {
+    parse: input => {
+      let JSONinput = input.trim();
+      // Fix JSON if leading and trailing brackets were trimmed.
+      if (JSONinput.substr(0, 1) !== '{') {
+        JSONinput = '{' + JSONinput;
+      }
+      if (JSONinput.substr(-1) !== '}') {
+        JSONinput = JSONinput + '}';
+      }
+      return JSONFormatter.fromFile(null, JSONinput);
+    },
+    stringify: output => JSONFormatter.toFile(null, output),
+  },
+  yaml: {
+    parse: input => YAML.fromFile(null, input),
+    stringify: output => YAML.toFile(null, output),
+  },
+};
+
+const getDelims = format => ({
+  yaml: '---',
+  toml: '+++',
+  json: ['{', '}'],
+}[format] || '---');
+
+function matterSettings(collectionOrEntity) {
+  const matterFormat = collectionOrEntity.get('front_matter_format', defaultFrontmatterFormat);
+  return { engines: parsers, language: matterFormat, delims: getDelims(matterFormat) };
 }
 
 export default {
   fromFile(collectionOrEntity, content) {
-    const result = matter(content, { engines: parsers, ...inferFrontmatterFormat(content) });
+    const result = matter(content, matterSettings(collectionOrEntity));
     const data = result.data;
     data.body = result.content;
     return data;
@@ -56,12 +60,6 @@ export default {
       }
     });
 
-    // always stringify to YAML
-    const parser = {
-      stringify(metadata) {
-        return YAML.toFile(metadata, sortedKeys);
-      },
-    };
-    return matter.stringify(body, meta, { language: "yaml", delimiters: "---", engines: { yaml: parser } });
+    return matter.stringify(body, meta, matterSettings(collectionOrEntity));
   },
 };
